f7559a5fbc8e386bd8d85daa262a973625dde08a
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-01-08 11:18:42 +0100

    finished fruit class

--- Nivell1-H2/S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/model/Fruit.java
+++ Nivell1-H2/S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/model/Fruit.java
@@ -1,2 +1,27 @@
-package cat.itacademy.s04.t02.n01.model;public class Fruit {
+package cat.itacademy.s04.t02.n01.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fruit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Setter
+    private String name;
+
+    @Setter
+    private int kgQuantity;
 }
