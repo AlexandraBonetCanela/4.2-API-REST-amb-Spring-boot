fe3ae61a3b8f799a5ecda99b75ec6d05af127b2f
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-01-10 12:24:37 +0100

    finished controlller

--- Nivell1-H2/S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/controller/FruitController.java
+++ Nivell1-H2/S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/controller/FruitController.java
@@ -1,2 +1,49 @@
-package cat.itacademy.s04.t02.n01.controller;public class FruitController {
+package cat.itacademy.s04.t02.n01.controller;
+
+import cat.itacademy.s04.t02.n01.model.Fruit;
+import cat.itacademy.s04.t02.n01.service.impl.FruitServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fruit")
+public class FruitController {
+
+    @Autowired
+    private FruitServiceImpl fruitService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Fruit> addFruit(@RequestBody Fruit fruit) {
+        Fruit newFruit = fruitService.addFruit(fruit);
+        return new ResponseEntity<>(newFruit, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Fruit> updateFruit(@RequestBody Fruit fruit) {
+        Fruit updatedFruit = fruitService.updateFruit(fruit);
+        return new ResponseEntity<>(updatedFruit, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteFruit(@PathVariable int id) {
+        fruitService.deleteFruit(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/getOne/{id}")
+    public ResponseEntity<Fruit> getFruit(@PathVariable int id) {
+        Fruit fruit = fruitService.getFruit(id);
+        return new ResponseEntity<>(fruit, HttpStatus.OK);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Fruit>> getAllFruits() {
+        List<Fruit> fruits = fruitService.getAllFruits();
+        return new ResponseEntity<>(fruits, HttpStatus.OK);
+    }
 }
